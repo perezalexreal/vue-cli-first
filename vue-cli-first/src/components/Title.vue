6baa15261099b8ecb172db9bd76ccdcb14bc8a70
perezalexreal <perezalexreal@gmail.com>
2022-01-26 15:41:43 +0100

    Title Vue

---
+++ vue-cli-first/src/components/Title.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1> La mia prima app con Vue CLI!</h1>
+    </div>
+</template>
+
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+h1 {
+    font-size: 18px;
+}
+</style>
